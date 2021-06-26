f15b2cb560fb48edcaa864ad92eb0d09cf820617
abhinav vp <vpofficial55@gmail.com>
2021-06-26 15:44:33 +0530

    bug fix

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {
-  BrowserRouter as Router
+  HashRouter as Router
 } from "react-router-dom";
 
 ReactDOM.render(
