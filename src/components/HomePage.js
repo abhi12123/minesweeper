c27cc250ae2e3cb6c5e98986599fba9a2c741289
abhinav <vpofficial55@gmail.com>
2022-02-07 16:19:09 +0530

    fixed bugs

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -10,14 +10,13 @@ export default function HomePage() {
             <div><Link to="/select-difficulty"><button className='hover-zoom'>Play</button></Link></div>
             <div><Link to="/how-to-play"><button className='hover-zoom'>How to Play</button></Link></div>
             <div className='icons'>
-                <i class="fab fa-google-play fa-2x"></i>
                 <i 
                     onClick={()=> window.open("https://github.com/abhi12123/minesweeper", "_blank")} 
                     className="hover-zoom cursor-pointer fab fa-github fa-2x"
                 ></i>
             </div>
             <div className='w3-small secondary-font footer-text'>created by 
-                <a className='secondary-font' href="https://www.linkedin.com/in/abhinavvp/" target="_blank"> Abhinav</a>
+                <a className='secondary-font' href="https://www.abhinavvp.com/" target="_blank"> Abhinav</a>
             </div>
         </div>
     )
--- src/components/HowToPlay.js
+++ src/components/HowToPlay.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { steps } from '../utils/steps';
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+// import "slick-carousel/slick/slick.css";
+// import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 import {
     Link,
